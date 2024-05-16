59c1be7879ce5af5c718c8e50f4748139d9f65df
Resos User <resos@nuwa.aerologie.net>
2024-05-16 15:38:36 +0200

    handle the nstart=0 case, add warning that it is not supported int he curr version

--- reprobus-simulation.sh
+++ reprobus-simulation.sh
@@ -18,9 +18,9 @@ function help() {
     echo "###       \`-....-'"
     echo "###"
     echo "### This script handles the REPROBUS simulation input"
-    echo "### parameters and launches the simulation, and the"
-    echo "### post-processing for the output additional"
-    echo "### reformating and results visualization"
+    echo "### parameters and launches the simulation, as well as"
+    echo "### the post-processing for the output additional"
+    echo "### formatting and results visualization"
     echo "###"
     echo "### Usage: ${SCRIPT_NAME} [options] arguments"
     echo "### Options:"
@@ -48,34 +48,34 @@ function help() {
     echo ""
 }
 
-function info_msg(){
+function info(){
 	txt=$1
 	echo "$(date +'%d/%m/%Y %H:%M:%S')   [INFO]   ${txt}"
 }
-function err_msg(){
+function error(){
 	txt=$1
 	echo "$(date +'%d/%m/%Y %H:%M:%S')   [ERROR]   ${txt}"
 }
-function warn_msg(){
+function warning(){
 	txt=$1
 	echo "$(date +'%d/%m/%Y %H:%M:%S')   [WARNING]   ${txt}"
 }
 
 function check_args(){
     if [[ "${COMPILER}" != "ifort" && "${COMPILER}" != "nvidia" ]]; then
-        err_msg "Unrecognized compiler options. Options are 'nvidia' or 'ifort', check your configuration file"
+        error "Unrecognized compiler options. Options are 'nvidia' or 'ifort', check your configuration file"
         exit_status=1
     fi
     if [[ ${START_DATE} =~ ^[0-9]{8}$ ]]; then
         exit_status=0
     else
-        err_msg "Starting date is not in the correct format YYYYMMDD"
+        error "Starting date is not in the correct format YYYYMMDD"
         exit_status=1
     fi
     if [[ ${END_DATE} =~ ^[0-9]{8}$ ]]; then
         exit_status=0
     else
-        err_msg "Ending date is not in the correct format YYYYMMDD"
+        error "Ending date is not in the correct format YYYYMMDD"
         exit_status=1
     fi
     return ${exit_status}
@@ -104,20 +104,20 @@ function prepare_directories(){
 
 function main(){
 
-    info_msg "!======================================================================!"
-    info_msg "!        _____  ______ _____  _____   ____  ____  _    _  _____        !"
-    info_msg "!       |  __ \|  ____|  __ \|  __ \ / __ \|  _ \| |  | |/ ____|       !"
-    info_msg "!       | |__) | |__  | |__) | |__) | |  | | |_) | |  | | (___         !"
-    info_msg "!       |  _  /|  __| |  ___/|  _  /| |  | |  _ <| |  | |\___ \        !"
-    info_msg "!       | | \ \| |____| |    | | \ \| |__| | |_) | |__| |____) |       !"
-    info_msg "!       |_|  \_\______|_|    |_|  \_\\\____/|____/ \____/|_____/        !"
-    info_msg "!======================================================================!"
+    info "!======================================================================!"
+    info "!        _____  ______ _____  _____   ____  ____  _    _  _____        !"
+    info "!       |  __ \|  ____|  __ \|  __ \ / __ \|  _ \| |  | |/ ____|       !"
+    info "!       | |__) | |__  | |__) | |__) | |  | | |_) | |  | | (___         !"
+    info "!       |  _  /|  __| |  ___/|  _  /| |  | |  _ <| |  | |\___ \        !"
+    info "!       | | \ \| |____| |    | | \ \| |__| | |_) | |__| |____) |       !"
+    info "!       |_|  \_\______|_|    |_|  \_\\\____/|____/ \____/|_____/        !"
+    info "!======================================================================!"
 
     prepare_directories
     cleanup
     cd ${WDIR}
 
-    info_msg "Compiling REPROBUS"
+    info "Compiling REPROBUS"
 
     ln -s /usr/local/REPROBUS/src/reprobus_l137_20230723.f ${WDIR}/reprobus_${EXP:2:4}.f
     ln -s /usr/local/REPROBUS/src/jno.f90 ${WDIR}/jno.f90
@@ -139,41 +139,45 @@ function main(){
     N_DAYS=$(count_days)
     DATE_IN=${START_DATE}
     DATE_OUT=${END_DATE}
-    info_msg "Simulation will be performed through ${NDAYS} days, from ${START_DATE} to ${END_DATE}"
+    info "Simulation will be performed through ${NDAYS} days, from ${START_DATE} to ${END_DATE}"
 
     while [ ${DATE_IN} -lt ${DATE_OUT} ]; do
 
         date_start=${DATE_IN}
         date_end=$(date -d "${DATE_IN} + 1 day" "+%Y%m%d")
 
-        info_msg "Preparing input data for the simulation"
+        info "Preparing input data for the simulation"
 
         ln -s ${DATA_DIR}/ecmwf_${date_start} ${WDIR}/ecmwf_${date_start}
         ln -s ${DATA_DIR}/ecmwf_${date_end} ${WDIR}/ecmwf_${date_end}
 
-        if [[ ${NSTART} == 1 ]]; then
+        if [[ ${NSTART} -eq 1 ]]; then
             ln -s ${RESTART_DIR}/MODEL_history_${date_start}12_${EXP} ${WDIR}/fort.90
         else
-            ln -s /usr/local/REPROBUS/src/qinit2d_l137_201006.20140331 ${WDIR}/qinit2d.txt
-            ln -s ${DATA_DIR}/ecmwf_o3_20221201 ${WDIR}/ecmwf_o3_20221201
+            # qinit_file=$(find /usr/local/REPROBUS/src -iname "qinit2d*")
+            # ln -s ${qinit_file} ${WDIR}/qinit2d.txt
+            # ln -s ${DATA_DIR}/ecmwf_o3_20240501 ${WDIR}/ecmwf_o3_20240501
+            warning "This case is not yet implemented, you might want to contact the model providers for more information"
+            warning "Exiting the script"
+            exit 1
         fi
 
-        info_msg "Working directory for the current date simulation"
+        info "Working directory for the current date simulation"
         ls -l ${WDIR}
 
-        info_msg "Launching REPROBUS"
+        info "Launching REPROBUS"
 
         ./reprobus_${EXP:2:4}
         status=$?
 
         if [[ ${status} == 0 ]]; then
-            info_msg "Simulation successful, continuing with the script"
+            info "Simulation successful, continuing with the script"
         else
-            err_msg "Something went wrong with the simulation, check the log messages above for more information"
+            error "Something went wrong with the simulation, check the log messages above for more information"
             exit 1
         fi
 
-        info_msg "Organizing output files"
+        info "Organizing output files"
 
         mv history ${RESTART_DIR}/MODEL_history_${date_end}12_${EXP}
         mv stations.txt ${RES_DIR}/stations_${date_end}12_${EXP}
@@ -218,13 +222,13 @@ while [[ $# -gt 0 ]]; do
 	case "$1" in
 		--config) shift; CONFIG_FILE=$1; shift;;
         -h|--help) help; exit 0; shift;;
-		\?) shift; err_msg "Unrecognized options"; exit 1; shift;;
+		\?) shift; error "Unrecognized options"; exit 1; shift;;
 		--) break;;
 	esac
 done
 
 if [[ -z ${CONFIG_FILE} ]]; then
-    err_msg "No configuration file was passed. Exiting script..."
+    error "No configuration file was passed. Exiting script..."
     exit 1
 fi
 
@@ -232,7 +236,7 @@ source ${CONFIG_FILE}
 check_args
 ARGS_STATUS=$?
 if [[ ${ARGS_STATUS} == 1 ]]; then
-    err_msg "Errors in input parameters detected, job was not launched"
+    error "Errors in input parameters detected, job was not launched"
     exit 1
 else
     main
